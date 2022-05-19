90ca78c93e7fd2ddc4cace9feeb98bc55469031d
acleret69 <aldrickcleret@gmail.com>
2022-05-19 17:49:08 +0200

    junit test complexity

--- src/main/java/org/ligoj/app/plugin/prov/ovh/catalog/OvhPriceImport.java
+++ src/main/java/org/ligoj/app/plugin/prov/ovh/catalog/OvhPriceImport.java
@@ -278,8 +278,10 @@ public class OvhPriceImport extends AbstractImportCatalogResource {
 							Function.identity()));
 			databasesAvaibility.stream()
 					.filter(c -> isEnabledEngine(context, c.getEngine())
-							&& isEnabledDatabaseType(context, c.getFlavor()) && enginesByName.containsKey(c.getEngine())
-							&& flavorsByName.containsKey(c.getFlavor()) && plansByName.containsKey(c.getPlan()))
+							&& isEnabledDatabaseType(context, c.getFlavor()) 
+							&& enginesByName.containsKey(c.getEngine())
+							&& flavorsByName.containsKey(c.getFlavor()) 
+							&& plansByName.containsKey(c.getPlan()))
 					.forEach(c -> installDatabasePrices(context, hourlyTerm, monthlyTerm, flavorsByName, plansByName,
 							pricesByCode, c));
 		}
